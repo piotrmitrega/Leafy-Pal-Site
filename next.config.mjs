59274046bd92d7386fb852e0c5aaf07aaad6d6f9
Piotr Mitrega <piotrmitrega1@gmail.com>
2025-06-08 22:47:47 +0200

    changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,4 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === "production";
-const isGitHubPages = process.env.GITHUB_PAGES === "true";
-
 const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
@@ -13,8 +10,7 @@ const nextConfig = {
     unoptimized: true,
   },
   output: "export",
-  basePath: isGitHubPages ? "/Leafy-Pal-Site" : "",
-  assetPrefix: isGitHubPages ? "/Leafy-Pal-Site/" : "",
+  trailingSlash: true,
 };
 
 export default nextConfig;
